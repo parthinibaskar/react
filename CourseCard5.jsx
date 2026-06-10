249871d4ea5ad3fb7332fecb8e1f2943c0a121ce
parthinibaskar <parthinib@gmail.com>
2026-06-10 22:42:12 +0530

    Add CourseCard5 component for web design course

---
+++ CourseCard5.jsx
@@ -0,0 +1,17 @@
+function CourseCard5() {
+  return (
+    <div className="card">
+      <img
+        src="https://thf.bing.com/th/id/OIP.etuCCaxXqVt-jwoAbvejpAHaEK?cb=thfc1falcon2&rs=1&pid=ImgDetMain&o=7&rm=3"
+        alt="Web design course"
+      />
+      <h2>Web Design</h2>
+      <p>Instructor: Ramya</p>
+      <p>Duration: 7 Weeks</p>
+      <p>Rating: ⭐ 4.8</p>
+      <p>Fee: ₹1899</p>
+    </div>
+  );
+}
+
+export default CourseCard5;
