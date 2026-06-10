5612a50821161ec6ac1e5469cd795097cc227b08
parthinibaskar <parthinib@gmail.com>
2026-06-10 22:39:20 +0530

    Add CourseCard1 component for React course display

---
+++ Coursecard1.jsx
@@ -0,0 +1,17 @@
+function CourseCard1() {
+  return (
+    <div className="card">
+      <img
+        src="https://ficustechnologies.com/wp-content/uploads/2023/11/react-1-1024x524.png"
+        alt="React course"
+      />
+      <h2>React Development</h2>
+      <p>Instructor: John </p>
+      <p>Duration: 8 Weeks</p>
+      <p>Rating: ⭐ 4.8</p>
+      <p>Fee: ₹1999</p>
+    </div>
+  );
+}
+
+export default CourseCard1;
