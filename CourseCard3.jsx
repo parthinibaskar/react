1c19cfc676607240a206257e8de846da57434a4f
parthinibaskar <parthinib@gmail.com>
2026-06-10 22:40:50 +0530

    Add CourseCard3 component for Python Basics course

---
+++ CourseCard3.jsx
@@ -0,0 +1,17 @@
+function CourseCard3() {
+  return (
+    <div className="card">
+      <img
+        src="https://blog.pwskills.com/wp-content/uploads/2025/02/Python-Basics-1024x576.jpg"
+        alt="Python course"
+      />
+      <h2>Python Basics</h2>
+      <p>Instructor: Mary</p>
+      <p>Duration: 6 Weeks</p>
+      <p>Rating: ⭐ 4.9</p>
+      <p>Fee: ₹1799</p>
+    </div>
+  );
+}
+
+export default CourseCard3;
