bf173bc58bdd9b2f6d8bdfe92aa8af1d61ab7d9d
parthinibaskar <parthinib@gmail.com>
2026-06-10 22:43:06 +0530

    Create CourseCard6 component for machine learning course

---
+++ CourseCard6.jsx
@@ -0,0 +1,17 @@
+function CourseCard6() {
+  return (
+    <div className="card">
+      <img
+        src="https://elearn.urfu.ru/pluginfile.php/1161832/course/overviewfiles/machine_learning_course_image.png?forcedownload=1"
+        alt="Machine learning course"
+      />
+      <h2>Machine Learning</h2>
+      <p>Instructor: Priya </p>
+      <p>Duration: 12 Weeks</p>
+      <p>Rating: ⭐ 4.9</p>
+      <p>Fee: ₹2999</p>
+    </div>
+  );
+}
+
+export default CourseCard6;
