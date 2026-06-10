264fd18d730e048b55e933faa91e8e9e6547c768
parthinibaskar <parthinib@gmail.com>
2026-06-10 22:41:32 +0530

    Add CourseCard4 component for C++ course

---
+++ CourseCard4.jsx
@@ -0,0 +1,17 @@
+function CourseCard4() {
+  return (
+    <div className="card">
+      <img
+        src="https://thf.bing.com/th/id/OIP.cQxvHA1HCKkWOSDhmKvIdwHaEK?cb=thfc1falcon2&rs=1&pid=ImgDetMain&o=7&rm=3"
+        alt="C++ course"
+      />
+      <h2>C++ Fundamentals</h2>
+      <p>Instructor: Michael </p>
+      <p>Duration: 9 Weeks</p>
+      <p>Rating: ⭐ 4.6</p>
+      <p>Fee: ₹2099</p>
+    </div>
+  );
+}
+
+export default CourseCard4;
