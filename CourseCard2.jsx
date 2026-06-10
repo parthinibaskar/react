2dc157cf34f962a701a48cb07d1b3c36b74cab7d
parthinibaskar <parthinib@gmail.com>
2026-06-10 22:40:03 +0530

    Create CourseCard2 component for Java course

---
+++ CourseCard2.jsx
@@ -0,0 +1,17 @@
+function CourseCard2() {
+  return (
+    <div className="card">
+      <img
+        src="https://i.ytimg.com/vi/A74TOX803D0/maxresdefault.jpg"
+        alt="Java course"
+      />
+      <h2>Java Programming</h2>
+      <p>Instructor: David </p>
+      <p>Duration: 10 Weeks</p>
+      <p>Rating: ⭐ 4.7</p>
+      <p>Fee: ₹2499</p>
+    </div>
+  );
+}
+
+export default CourseCard2;
