f7fac1de94825cb5bfee3f4186b762c1fc174870
parthinibaskar <parthinib@gmail.com>
2026-06-10 22:37:19 +0530

    Create App component to render course cards

---
+++ App.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+import CourseCard1 from "./CourseCard1";
+import CourseCard2 from "./CourseCard2";
+import CourseCard3 from "./CourseCard3";
+import CourseCard4 from "./CourseCard4";
+import CourseCard5 from "./CourseCard5";
+import CourseCard6 from "./CourseCard6";
+
+function App() {
+  return (
+    <div className="container">
+      <CourseCard1 />
+      <CourseCard2 />
+      <CourseCard3 />
+      <CourseCard4 />
+      <CourseCard5 />
+      <CourseCard6 />
+    </div>
+  );
+}
+
+export default App;
